68cf02aec1ebfef78d154269f30d25247fb1a265
String-Xeno <the1flying2gamer345@gmail.com>
2024-10-30 17:15:35 +0200

    class 30-10, working with modulo for the first time and learning how to swap out values from left to right

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("how long is the number?");
+        Scanner input = new Scanner(System.in);
+        //int timestorepeat = input.nextInt();
+        System.out.println("what is the number?");
+        int num = input.nextInt();
+        int temp;
+        int num1 = 0;
+        while (num%10 > 0) {
+            temp = num % 10;
+            num1 = num1*10 + temp;
+            num = num/10;
+        }
+        System.out.println(num1);
+    }
+}
